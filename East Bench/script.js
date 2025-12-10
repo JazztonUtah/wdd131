8be43581ebbb812c924e9a43ae2955238c6c18fb
JazztonUtah <ortondevin@gmail.com>
2025-12-10 10:24:56 -0700

    more updates

--- East Bench/script.js
+++ East Bench/script.js
@@ -2,47 +2,47 @@
 const roomData = {
     'chapel': {
         name: 'Chapel',
-        image: 'images/chapel.png',
-        youthAdult: 'Recommended: 200-300',
+        image: 'images/chapel.jpeg',
+        youthAdult: 'Recommended: 200',
         primaryKids: 'N/A',
         classes: 'Sacrament Meeting',
-        teachers: ''
+        teachers: 'N/A'
     },
     'overflow-1': {
         name: 'Overflow 1',
-        image: '',
-        youthAdult: 'Recommended: 50-75',
+        image: 'images/overflow1.jpeg',
+        youthAdult: 'Recommended: 100',
         primaryKids: 'N/A',
         classes: '',
         teachers: ''
     },
     'overflow-2': {
         name: 'Overflow 2',
-        image: '',
-        youthAdult: 'Recommended: 25-40',
+        image: 'images/overflow2.jpeg',
+        youthAdult: 'Recommended: 50',
         primaryKids: 'N/A',
         classes: '',
         teachers: ''
     },
     'room-107': {
         name: 'Room 107',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/107.jpeg',
+        youthAdult: '14 adults',
+        primaryKids: '16 kids',
         classes: '',
         teachers: ''
     },
     'room-108': {
         name: 'Room 108',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/108.jpeg',
+        youthAdult: '14 adults',
+        primaryKids: '16 kids',
         classes: '',
         teachers: ''
     },
     'bathroom': {
         name: 'Bathroom',
-        image: '',
+        image: 'images/washroom.jpeg',
         youthAdult: 'N/A',
         primaryKids: 'N/A',
         classes: 'N/A',
@@ -50,7 +50,7 @@ const roomData = {
     },
     'north-foyer': {
         name: 'North Foyer',
-        image: '',
+        image: 'images/northfoyer.jpeg',
         youthAdult: 'N/A',
         primaryKids: 'N/A',
         classes: '',
@@ -58,7 +58,7 @@ const roomData = {
     },
     'south-foyer': {
         name: 'South Foyer',
-        image: '',
+        image: 'images/southfoyer.jpeg',
         youthAdult: 'N/A',
         primaryKids: 'N/A',
         classes: '',
@@ -74,23 +74,23 @@ const roomData = {
     },
     'rs-room': {
         name: 'RS Room',
-        image: '',
-        youthAdult: '',
-        primaryKids: 'N/A',
+        image: 'images/RS.jpeg',
+        youthAdult: '50 adults',
+        primaryKids: '60 kids',
         classes: 'Relief Society',
         teachers: ''
     },
     'primary-room': {
         name: 'Primary Room',
-        image: '',
-        youthAdult: 'N/A',
-        primaryKids: '',
+        image: 'images/primary.jpeg',
+        youthAdult: '50 adults',
+        primaryKids: '60 kids',
         classes: 'Primary',
         teachers: ''
     },
     'bishop-nw': {
         name: 'Bishop Office (Our Ward)',
-        image: '',
+        image: 'images/ourbishop.jpeg',
         youthAdult: '2-5',
         primaryKids: 'N/A',
         classes: 'Bishop Meetings',
@@ -98,7 +98,7 @@ const roomData = {
     },
     'clerk-nw': {
         name: 'Clerk Office (Our Ward)',
-        image: '',
+        image: 'images/ourclerk.jpeg',
         youthAdult: '2-4',
         primaryKids: 'N/A',
         classes: 'Clerk Duties',
@@ -106,7 +106,7 @@ const roomData = {
     },
     'bishop-nw-2': {
         name: 'Bishop Office',
-        image: '',
+        image: 'images/bishopnorth.jpeg',
         youthAdult: '2-5',
         primaryKids: 'N/A',
         classes: 'Bishop Meetings',
@@ -114,7 +114,7 @@ const roomData = {
     },
     'clerk-nw-2': {
         name: 'Clerk Office',
-        image: '',
+        image: 'images/clerknorth.jpeg',
         youthAdult: '2-4',
         primaryKids: 'N/A',
         classes: 'Clerk Duties',
@@ -122,31 +122,31 @@ const roomData = {
     },
     'room-101': {
         name: 'Room 101',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/101.jpeg',
+        youthAdult: '10 adults',
+        primaryKids: '12 kids',
         classes: '',
         teachers: ''
     },
     'room-102': {
         name: 'Room 102',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/102.jpeg',
+        youthAdult: '6 adults',
+        primaryKids: '8 kids',
         classes: '',
         teachers: ''
     },
     'room-103': {
         name: 'Room 103',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/103.jpeg',
+        youthAdult: '10 adults',
+        primaryKids: '12 kids',
         classes: '',
         teachers: ''
     },
     'bathroom-north': {
         name: 'Bathroom',
-        image: '',
+        image: 'images/washroom.jpeg',
         youthAdult: 'N/A',
         primaryKids: 'N/A',
         classes: 'N/A',
@@ -154,7 +154,7 @@ const roomData = {
     },
     'custodial': {
         name: 'Custodial Closet',
-        image: '',
+        image: 'images/custodial.jpeg',
         youthAdult: 'N/A',
         primaryKids: 'N/A',
         classes: 'N/A',
@@ -162,7 +162,7 @@ const roomData = {
     },
     'storage': {
         name: 'Table Storage',
-        image: '',
+        image: 'images/storage.jpeg',
         youthAdult: 'N/A',
         primaryKids: 'N/A',
         classes: 'N/A',
@@ -170,7 +170,7 @@ const roomData = {
     },
     'kitchen': {
         name: 'Kitchen',
-        image: '',
+        image: 'images/kitchen.jpeg',
         youthAdult: 'N/A',
         primaryKids: 'N/A',
         classes: 'N/A',
@@ -178,31 +178,31 @@ const roomData = {
     },
     'room-104': {
         name: 'Room 104',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/104.jpeg',
+        youthAdult: '14 adults',
+        primaryKids: '16 kids',
         classes: '',
         teachers: ''
     },
     'room-105': {
         name: 'Room 105',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/105.jpeg',
+        youthAdult: '14 adults',
+        primaryKids: '16 kids',
         classes: '',
         teachers: ''
     },
     'room-106': {
         name: 'Room 106',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/106.jpeg',
+        youthAdult: '14 adults',
+        primaryKids: '16 kids',
         classes: '',
         teachers: ''
     },
     'mothers': {
         name: "Mother's Room",
-        image: '',
+        image: 'images/mothers.jpeg',
         youthAdult: 'N/A',
         primaryKids: 'N/A',
         classes: 'N/A',
@@ -218,55 +218,55 @@ const roomData = {
     },
     'room-115': {
         name: 'Room 115',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/115.jpeg',
+        youthAdult: '10 adults',
+        primaryKids: '12 kids',
         classes: '',
         teachers: ''
     },
     'room-114': {
         name: 'Room 114',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/114.jpeg',
+        youthAdult: '10 adults',
+        primaryKids: '12 kids',
         classes: '',
         teachers: ''
     },
     'room-113': {
         name: 'Room 113',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/113.jpeg',
+        youthAdult: '10 adults',
+        primaryKids: '12 kids',
         classes: '',
         teachers: ''
     },
     'room-112': {
         name: 'Room 112',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        image: 'images/112.jpeg',
+        youthAdult: '10 adults',
+        primaryKids: '12 kids',
         classes: '',
         teachers: ''
     },
     'nursery': {
         name: 'Nursery',
-        image: '',
+        image: 'images/nursery.jpeg',
         youthAdult: 'N/A',
-        primaryKids: '',
+        primaryKids: '14 kids',
         classes: 'Nursery',
         teachers: ''
     },
-    'room-110': {
-        name: 'Room 110',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+    'room-109': {
+        name: 'Room 109',
+        image: 'images/109.jpeg',
+        youthAdult: '6 adults',
+        primaryKids: '8 kids',
         classes: '',
         teachers: ''
     },
     'bishop-sw': {
         name: 'Bishop Office',
-        image: '',
+        image: 'images/bishopsouth.jpeg',
         youthAdult: '2-5',
         primaryKids: 'N/A',
         classes: 'Bishop Meetings',
@@ -274,23 +274,24 @@ const roomData = {
     },
     'clerk-sw': {
         name: 'Clerk Office',
-        image: '',
+        image: 'images/clerksouth.jpeg',
         youthAdult: '2-4',
         primaryKids: 'N/A',
         classes: 'Clerk Duties',
         teachers: ''
     },
     'old-hp-room': {
-        name: 'Old HP Room',
-        image: '',
-        youthAdult: '',
-        primaryKids: '',
+        name: 'Room 116 & 117',
+        image: 'images/116.jpeg',
+        youthAdult: '16 adults',
+        primaryKids: '20 kids',
         classes: '',
-        teachers: ''
+        teachers: '',
+        note: 'are the same room and cannot be split.'
     },
     'room-116': {
         name: 'Room 116',
-        image: '',
+        image: 'images/clk.jpeg',
         youthAdult: '',
         primaryKids: '',
         classes: '',
@@ -298,7 +299,7 @@ const roomData = {
     },
     'room-117': {
         name: 'Room 117',
-        image: '',
+        image: 'images/ysa.jpeg',
         youthAdult: '',
         primaryKids: '',
         classes: '',
@@ -332,7 +333,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Open modal function
 function openModal(data) {
-    modalTitle.textContent = data.name;
+    if (data.note) {
+        modalTitle.innerHTML = data.name + ' <span class="modal-note-inline">- ' + data.note + '</span>';
+    } else {
+        modalTitle.textContent = data.name;
+    }
     
     // Set image
     if (data.image) {
